d6d4de6c5adeb11f620f7b83019751b3aab7805c
kishorkumar11 <h.kishorkumar7@gmail.com>
2024-04-07 15:28:45 +0800

    Fix route bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 // import HelloWorld from "./Components/HelloWorld";
-import Home1 from "./components/Pages/HomePage/HomePage";
+import Home1 from './components/Pages/HomePage/HomePage.jsx';
 // import Home2 from "./components/Pages/HomePage2/HomePage";
 import ResultsPageSingle1Col from "./components/Pages/ResultsPageSingle1Col";
 
@@ -8,7 +8,7 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Home1 />} />
-            <Route path="/" element={<Home2 />} />
+            {/* <Route path="/home2" element={<Home2 />} /> */}
             <Route path="/results-1" element={<ResultsPageSingle1Col />} />
         </Routes>
     );
