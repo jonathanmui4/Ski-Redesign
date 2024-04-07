22a5e66572ced3c3122156f13fd9652a03f78ccd
kishorkumar11 <h.kishorkumar7@gmail.com>
2024-04-07 11:29:10 +0800

    Finish home page centralised

--- src/Components/Pages/HomePage/Card.jsx
+++ src/Components/Pages/HomePage/Card.jsx
@@ -25,8 +25,8 @@ const Card = ({
 
   return (
     <div className="card">
-      <div className="image" style={imageStyle}>
-        <div className="top-aligned-0px-wrapper">
+      <div className="image">
+        {/* <div className="top-aligned-0px-wrapper">
           <div className="favorite-border-wrapper">
             <img
               className="favorite-border-icon"
@@ -35,8 +35,8 @@ const Card = ({
               src={favoriteBorder}
             />
           </div>
-        </div>
-        <div className="center-aligned-0px-wrapper">
+        </div> */}
+        {/* <div className="center-aligned-0px-wrapper">
           <div className="wrapper">
             <div className="vertical-shift-50-height">
               <div className="aspect-ratio-keeper-rotated" />
@@ -51,8 +51,8 @@ const Card = ({
               </div>
             </div>
           </div>
-        </div>
-        <div className="aspect-ratio">
+        </div> */}
+        <div className="aspect-ratio" style={imageStyle}>
           <img
             className="aspect-ratio-keeper-addition"
             loading="lazy"
