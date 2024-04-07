e5f138a74a7aae343610f79d195e456ebf574fb1
kishorkumar11 <h.kishorkumar7@gmail.com>
2024-04-07 15:08:03 +0800

    Implement home page centralised

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 // import HelloWorld from "./Components/HelloWorld";
 import Home1 from "./components/Pages/HomePage/HomePage";
+// import Home2 from "./components/Pages/HomePage2/HomePage";
 import ResultsPageSingle1Col from "./components/Pages/ResultsPageSingle1Col";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home1 />} />
+            <Route path="/" element={<Home2 />} />
             <Route path="/results-1" element={<ResultsPageSingle1Col />} />
         </Routes>
     );
